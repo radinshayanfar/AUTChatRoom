b97cf0ea4ab6aae2009b38cc53f99f7c28f3c169
Radin Shayanfar <radin.shayanfar@gmail.com>
2019-05-26 22:04:55 +0430

    client's main import optimized

--- src/ChatRoom/client/Main.java
+++ src/ChatRoom/client/Main.java
@@ -3,8 +3,6 @@ package ChatRoom.client;
 import ChatRoom.client.controller.LoginController;
 import ChatRoom.client.model.ServerConfig;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         String host = "127.0.0.1";
